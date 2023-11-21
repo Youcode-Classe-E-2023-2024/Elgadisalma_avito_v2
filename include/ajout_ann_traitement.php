9318011085328b8cd950f7c83849add4d711c3ff
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-21 20:29:12 +0100

    Create ajout_ann_traitement.php

---
+++ include/ajout_ann_traitement.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once './include/config.php';
+session_start();
+
+if (!isset($_SESSION['id'])) {
+    header("location: ../pages/index.php");
+    exit();
+}
+
+
+
+if (isset($_POST['submit'])) {
+    $nom_utilisateur = $_SESSION['nom_utilisateur'];
+    $numero_tel = $_POST['phoneNumber'];
+    $titre_annonce = $_POST['annoceTitre'];
+    $description_annonce = $_POST['annoceDescription'];
+    $date_ajout = $_POST['dateAjout'];
+
+    $sql_insert = "INSERT INTO annonce (nom_utilisateur, numero_tel, titre_annonce, description_annonce, date_ajout) 
+                   VALUES ('$nom_utilisateur', '$numero_tel', '$titre_annonce', '$description_annonce', '$date_ajout')";
+
+
+    if ($link->query($sql_insert) === TRUE) {
+        header("location:../pages/annonce.php?task=added_successfully");
+        exit();
+    } else {
+        echo "Erreur: " . $link->error;
+    }
+}
+
+    $link->close();
+
+?>
