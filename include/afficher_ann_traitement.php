b1c2a1f0e76562b16b01c4a493e84f78149977de
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-21 20:29:09 +0100

    Create afficher_ann_traitement.php

---
+++ include/afficher_ann_traitement.php
@@ -0,0 +1,22 @@
+<?php
+function get_annonce() {
+    require_once './include/config.php';
+        $annonce = [];
+    
+    
+        $sql = "SELECT * FROM annonce  ;";
+        $result = $link->query($sql);
+        if(mysqli_num_rows($result) > 0){
+            while ($row = $result->fetch_assoc()) {
+                $annonce[] = $row;
+            }
+        }else {
+            echo "Aucune annonce trouvée.";
+        }
+        mysqli_free_result($result);
+        mysqli_close($link);
+
+    
+    return $annonce;
+
+}
