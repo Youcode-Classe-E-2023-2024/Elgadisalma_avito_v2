55846f2a531bc08527920b19ac1601e76452b59e
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-21 20:59:42 +0100

    Update mes_ann_traitement.php

--- include/mes_ann_traitement.php
+++ include/mes_ann_traitement.php
@@ -3,9 +3,7 @@
 
 function get_mes_annonces() {
 
-    // session_start();
-
-    require_once './include/config.php';
+    require_once '../config.php';
     $nom_utilisateur = isset($_SESSION['nom_utilisateur']) ? $_SESSION['nom_utilisateur'] : '';
 
     $sql = "SELECT * FROM annonce WHERE nom_utilisateur = ?";
