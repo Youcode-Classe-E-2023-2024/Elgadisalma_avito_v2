f212efa8883c06262dfdc3c5a5ec2e764666a981
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-21 20:29:31 +0100

    Create mes_ann_traitement.php

---
+++ include/mes_ann_traitement.php
@@ -0,0 +1,29 @@
+<?php
+
+
+function get_mes_annonces() {
+
+    // session_start();
+
+    require_once './include/config.php';
+    $nom_utilisateur = isset($_SESSION['nom_utilisateur']) ? $_SESSION['nom_utilisateur'] : '';
+
+    $sql = "SELECT * FROM annonce WHERE nom_utilisateur = ?";
+    $stmt = $link->prepare($sql);
+
+    $stmt->bind_param("s", $nom_utilisateur);
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    $stmt->close();
+
+    $annonces = [];
+    while ($row = $result->fetch_assoc()) {
+        $annonces[] = $row;
+    }
+
+    return $annonces;
+}
+?>
