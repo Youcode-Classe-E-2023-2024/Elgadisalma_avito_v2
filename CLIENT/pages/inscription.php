85a77ca2c8eb787d743f9491af372bab8c5da55c
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-23 10:52:03 +0100

    Update inscription.php

--- CLIENT/pages/inscription.php
+++ CLIENT/pages/inscription.php
@@ -6,37 +6,42 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <title>S'inscrire à Avito </title>
+    <!-- <link rel="stylesheet" href="style.css"> -->
 </head>
 
 <body>
+        <?php
+         require_once 'navbar2.php';
+        ?>
+
     <h1>Bienvenue à Avito</h1>
     <form action="../include/Add_user.php" method="post">
 
-      <div class="mb-3 row">
-    <label for="staticEmail" class="col-sm-2 col">nom d'utilisateur</label>
-    <div class="col-sm-10">
-      <input type="text"  class="form-control" id="staticEmail" name="userName">
-      <span class="text-danger"><?php if(isset($_GET['STATUS']))echo $_GET['STATUS'] ?></span>
+      <div class="input-group flex-nowrap">
+        <span class="input-group-text" id="addon-wrapping">+</span>
+        <input type="text" class="form-control" placeholder="Nom d'utilisateur" aria-describedby="addon-wrapping" name='userName'>
+        <span class="text-danger"><?php if(isset($_GET['STATUS']))echo $_GET['STATUS'] ?></span>
+    </div>
+    <br>
+      
     </div>
       </div>
 
-    <div class="mb-3 row">
-      <label for="text" class="col-sm-2 col-form-label">numero de telephone</label>
-      <div class="col-sm-10">
-        <input type="text" class="form-control" id="numero_tel" name="numero_tel">
-      </div>
+      <div class="input-group flex-nowrap">
+        <span class="input-group-text" id="addon-wrapping">+</span>
+        <input type="text" class="form-control" placeholder="Numéro de télephone" aria-describedby="addon-wrapping" name='numero_tel'>
     </div>
+    <br>
 
-    <div class="mb-3 row">
-      <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-      <div class="col-sm-10">
-        <input type="password" class="form-control" id="inputPassword" name="password">
-      </div>
-    </div>
+    <div class="input-group flex-nowrap">
+      <span class="input-group-text" id="addon-wrapping">+</span>
+      <input type="password" class="form-control" placeholder="Mot de passe" aria-describedby="addon-wrapping" name='password'>
+  </div>
+  <br>
 
-    <div class="col-sm-2 col-form-label">
-      <button>S'inscrire</button>
-    </div>
+  <div class="d-grid gap-2">
+    <button type="submit" class="btn btn-danger">S'inscrire</button>
+</div
   
     </form>
     
