46c23d3b7f10622aef4ee7d33b14de8db9274223
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-23 19:50:43 +0100

    Update connect_user.php

--- CLIENT/include/connect_user.php
+++ CLIENT/include/connect_user.php
@@ -5,7 +5,7 @@ require_once 'config.php';
 $nom_utilisateur = $_POST['userName'];
 $password = $_POST['password'];
 
-$sql_check_user = "SELECT id, nom_utilisateur, password FROM utilisateur WHERE nom_utilisateur = '$nom_utilisateur' && password = '$password'";
+$sql_check_user = "SELECT id, nom_utilisateur, role FROM utilisateur WHERE nom_utilisateur = '$nom_utilisateur' && password = '$password'";
 $result_check_user = $link->query($sql_check_user);
 
 if (!$result_check_user) { 
@@ -15,18 +15,21 @@ if (!$result_check_user) {
 if ($result_check_user->num_rows > 0) {
     $user = $result_check_user->fetch_assoc();
 
-        session_start();
-        $_SESSION['id'] = $user['id'];
-        $_SESSION['nom_utilisateur'] = $user['nom_utilisateur'];
-        $_SESSION['numero_tel'] = $user['numero_tel'];
+    session_start();
+    $_SESSION['id'] = $user['id'];
+    $_SESSION['nom_utilisateur'] = $user['nom_utilisateur'];
+    $_SESSION['role'] = $user['role'];
 
-        header("Location: ../pages/index.php");
-        exit();
+    if ($user['role'] == 'admin') {
+        header("Location: ../pages/indexA.php");
     } else {
-        header("Location: ../pages/connexion.php?STATUS=probleme de connexion");
-        exit();
+        header("Location: ../pages/index.php");
     }
-
+    exit();
+} else {
+    header("Location: ../pages/connexion.php?STATUS=probleme de connexion");
+    exit();
+}
 
 // Fermer la connexion
 $link->close();
