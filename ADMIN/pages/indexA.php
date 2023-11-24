6b4ccc4d051e53e41a7a064d4d639c6f98ff0d0c
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-24 14:57:36 +0100

    Update indexA.php

--- ADMIN/pages/indexA.php
+++ ADMIN/pages/indexA.php
@@ -29,7 +29,54 @@ session_start();
    } ?>
 <div class="container mt-5">
     <h1>Bonjour <?php echo $_SESSION['nom_utilisateur']; ?></h1>
-    <h2>Liste d'annonce</h2>
+<h2>Liste d'annonces non approuvées</h2>
+<br>
+    
+    <table class="table mt-3">
+        <thead>
+            <tr>
+                <th>Vendeur</th>
+                <th>Numero de telephone</th>
+                <th>Titre du produit</th>
+                <th>État</th>
+                <th>Description du produit</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        
+        <tbody id="taskTableBody">
+        <?php
+            include_once '../include/afficher_ann_A.php';
+            $annonces = get_annonce_refuse();
+            foreach ($annonces as $annonce) {
+        ?>
+            <tr>
+                <td><?php echo $annonce['nom_utilisateur'] ;?></td>
+                <td><?php echo $annonce['numero_tel'] ;?> </td>
+                <td><?php echo $annonce['titre_annonce'] ;?> </td>
+                <td><?php echo $annonce['etat_annonce'] ;?> </td>
+                <td><?php echo $annonce['description_annonce'] ;?> </td>
+                <td><form action="../include/refuse.php" method="get" style="display: inline;">
+                    <input type="hidden" name="id" value="<?php echo $annonce['id']; ?>">
+                    <button type="submit" class="btn btn-danger btn-sm">
+                        <i class="fas fa-trash"></i> Refuser
+                    </button>
+                </form>
+            </td>
+            <td><form action="../include/approuve.php" method="get" style="display: inline;">
+                    <input type="hidden" name="id" value="<?php echo $annonce['id']; ?>">
+                    <button class="btn btn-warning btn-sm">
+                        <i class="fas fa-edit"></i> Approuver
+                    </button>
+                </form>
+            </td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+
+
+    <h2>Liste d'annonces approuvées</h2>
     
     <br>
     
@@ -48,7 +95,7 @@ session_start();
         <tbody id="taskTableBody">
         <?php
             include_once '../include/afficher_ann_A.php';
-            $annonces = get_annonce();
+            $annonces = get_annonce_approuve();
             foreach ($annonces as $annonce) {
         ?>
             <tr>
