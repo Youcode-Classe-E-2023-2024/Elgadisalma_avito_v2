64932ba820097d7ee217aa055a89932493a7d8c8
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-24 15:33:38 +0100

    Update afficher_ann_A.php

--- ADMIN/include/afficher_ann_A.php
+++ ADMIN/include/afficher_ann_A.php
@@ -1,41 +1,27 @@
 <?php
-function get_annonce_refuse() {
-    require_once 'config.php';
-        $annonce = [];
-    
-    
-        $sql = "SELECT * FROM annonce WHERE statut='refuse' ;";
-        $result = $link->query($sql);
-        if(mysqli_num_rows($result) > 0){
+require_once 'config.php';
+
+function get_annonces_par_statut($link, $statut) {
+    $annonce = [];
+    $sql = "SELECT * FROM annonce WHERE statut='$statut';";
+    $result = $link->query($sql);
+
+    if ($result) {
+        if (mysqli_num_rows($result) > 0) {
             while ($row = $result->fetch_assoc()) {
                 $annonce[] = $row;
             }
-        }else {
+        } else {
             echo "Aucune annonce trouvée.";
         }
         mysqli_free_result($result);
-        mysqli_close($link);
+    } else {
+        echo "Erreur dans la requête : " . mysqli_error($link);
+    }
 
-    
+   
+
     return $annonce;
-
 }
-function get_annonce_approuve(){
-    require_once 'config.php';
-        $annonce = [];
-    
-    
-        $sql = "SELECT * FROM annonce WHERE statut='approuve' ;";
-        $result = $link->query($sql);
-        if(mysqli_num_rows($result) > 0){
-            while ($row = $result->fetch_assoc()) {
-                $annonce[] = $row;
-            }
-        }else {
-            echo "Aucune annonce trouvée.";
-        }
-        mysqli_free_result($result);
-        mysqli_close($link);
 
-    
-    return $annonce;}
+//  mysqli_close($link);  
