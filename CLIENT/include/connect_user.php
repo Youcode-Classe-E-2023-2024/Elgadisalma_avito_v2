d070909c6ec3f57f6cd068cd126de1b02e6089db
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-24 17:28:09 +0100

    Update connect_user.php

--- CLIENT/include/connect_user.php
+++ CLIENT/include/connect_user.php
@@ -1,11 +1,10 @@
 <?php
-
 require_once 'config.php';
 
 $nom_utilisateur = $_POST['userName'];
 $password = $_POST['password'];
 
-$sql_check_user = "SELECT id, nom_utilisateur, role FROM utilisateur WHERE nom_utilisateur = '$nom_utilisateur' && password = '$password'";
+$sql_check_user = "SELECT id, nom_utilisateur, role, password FROM utilisateur WHERE nom_utilisateur = '$nom_utilisateur' ";
 $result_check_user = $link->query($sql_check_user);
 
 if (!$result_check_user) { 
@@ -15,17 +14,23 @@ if (!$result_check_user) {
 if ($result_check_user->num_rows > 0) {
     $user = $result_check_user->fetch_assoc();
 
-    session_start();
-    $_SESSION['id'] = $user['id'];
-    $_SESSION['nom_utilisateur'] = $user['nom_utilisateur'];
-    $_SESSION['role'] = $user['role'];
+    // Vérifier le mot de passe haché
+    if (password_verify($password, $user['password'])) {
+        session_start();
+        $_SESSION['id'] = $user['id'];
+        $_SESSION['nom_utilisateur'] = $user['nom_utilisateur'];
+        $_SESSION['role'] = $user['role'];
 
-    if ($user['role'] == 'admin') {
-        header("Location: ../../ADMIN/pages/indexA.php");
+        if ($user['role'] == 'admin') {
+            header("Location: ../../ADMIN/pages/indexA.php");
+        } else {
+            header("Location: ../pages/index.php");
+        }
+        exit();
     } else {
-        header("Location: ../pages/index.php");
+        header("Location: ../pages/connexion.php?STATUS=probleme de connexion");
+        exit();
     }
-    exit();
 } else {
     header("Location: ../pages/connexion.php?STATUS=probleme de connexion");
     exit();
