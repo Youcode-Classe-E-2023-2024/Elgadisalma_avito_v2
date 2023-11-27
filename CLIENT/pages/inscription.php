7259d04002793b809f6f4bde55aa45e6129a0b66
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-27 16:58:53 +0100

    Update inscription.php

--- CLIENT/pages/inscription.php
+++ CLIENT/pages/inscription.php
@@ -17,7 +17,7 @@
         <div class="container">
     <h1>Bienvenue à Avito</h1>
     <h6>Inscrivez-vous pour profiter d'offres exclusives</h6>
-    <form action="../include/Add_user.php" method="post">
+    <form action="../include/Add_user.php" method="post" enctype="multipart/form-data">
 
       <div class="input-group flex-nowrap">
         <span class="input-group-text" id="addon-wrapping">+</span>
@@ -37,8 +37,14 @@
       <span class="input-group-text" id="addon-wrapping">+</span>
       <input type="password" class="form-control" placeholder="Mot de passe" aria-describedby="addon-wrapping" name='password'>
   </div>
+
+  <div class="flex flex-col justify-center items-center">
+    <!-- <img src="pictures/add.png" alt="add image" width="70" height="70" class="cursor-pointer" id="add">
+    <img src="pictures/done.png" alt="image added" width="70" height="70" class="cursor-pointer hidden" id="done"> -->
+    <input type="file" id="photo"  placeholder="Photo de profil"  class="input-group-text" name='photo'>
+</div>
   <br>
-  <div class="botonaaata">
+  <!-- <div class="botonaaata"> -->
   <div class="d-grid gap-2">
     <button type="submit" class="btn btn-danger">S'inscrire</button>
   </div>
