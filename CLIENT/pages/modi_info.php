1b04953c3582a4fbe016172d2459b6b51edfb106
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-28 14:13:24 +0100

    Update modi_info.php

--- CLIENT/pages/modi_info.php
+++ CLIENT/pages/modi_info.php
@@ -3,7 +3,7 @@ session_start();
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -25,24 +25,25 @@ session_start();
             <div class="ph_profil">
                 <img src="../include/uploads/<?php echo $_SESSION['photo']; ?>" alt="Photo de profil">
             </div>
+
             <div class="info">
-            <div class="col-md-4">
-            <form class="row g-3 needs-validation" action="../include/edit_traitement.php" method="post">
-        <div class="col-md-4">
-            <label for="validationCustom02" class="form-label">Nom d'utilisateur</label>
-            <input type="text" class="form-control" id="validationCustom02" name="nom_utilisateur" value="<?php echo $row['nom_utilisateur'] ?>" required>
-        </div>
+            <form class="row g-3 needs-validation" action="../include/edit_me.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $_SESSION['id']; ?>">
+                <div class="col-md-4">
+                    <label for="validationCustom02" class="form-label">Nom d'utilisateur</label>
+                    <input type="text" class="form-control" id="validationCustom02" name="nom_utilisateur" value="<?php echo $_SESSION['nom_utilisateur']; ?>" required>
+                </div>
 
-            <label for="validationCustom01" class="form-label">Numero de telephone </label>
-            <input type="text" class="form-control" id="validationCustom01" name="numero_tel" value="<?php echo $row['numero_tel'] ?>" required>
-        </div>
+                <div class="col-md-4">
+                    <label for="validationCustom01" class="form-label">Numero de telephone </label>
+                    <input type="text" class="form-control" id="validationCustom01" name="numero_tel" value="<?php echo $_SESSION['numero_tel']; ?>" required>
+                </div>
+                <button class="btn btn-primary" type="submit" name="submit">Enregistrer les modifications</button>
+            </form>
+            </div>
+        
 
-        <form action="../include/delete_me.php" method="get" style="display: inline;">
-            <input type="hidden" name="deleteUser" value="<?php echo $_SESSION['id']; ?>">
-            <button type="submit" class="btn btn-danger btn-sm">
-                <i class="fas fa-trash"></i> Enregistrer les modifications
-            </button>
-        </form>
+        
                         
             </div>
         </div>
