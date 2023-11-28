498e972e95727cb19745e7d9b05e4bfc89831672
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-28 15:14:07 +0100

    Update modi_info.php

--- CLIENT/pages/modi_info.php
+++ CLIENT/pages/modi_info.php
@@ -27,18 +27,22 @@ session_start();
             </div>
 
             <div class="info">
-            <form class="row g-3 needs-validation" action="../include/edit_me.php" method="post">
+            <form  action="../include/edit_me.php" method="post">
             <input type="hidden" name="id" value="<?php echo $_SESSION['id']; ?>">
-                <div class="col-md-4">
-                    <label for="validationCustom02" class="form-label">Nom d'utilisateur</label>
+                <div class="col-md-10">
+                    <label for="validationCustom02" class="form-label">*Nom d'utilisateur</label>
                     <input type="text" class="form-control" id="validationCustom02" name="nom_utilisateur" value="<?php echo $_SESSION['nom_utilisateur']; ?>" required>
                 </div>
-
-                <div class="col-md-4">
-                    <label for="validationCustom01" class="form-label">Numero de telephone </label>
+<br>
+                <div class="col-md-10">
+                    <label for="validationCustom01" class="form-label">*Numero de telephone </label>
                     <input type="text" class="form-control" id="validationCustom01" name="numero_tel" value="<?php echo $_SESSION['numero_tel']; ?>" required>
                 </div>
-                <button class="btn btn-primary" type="submit" name="submit">Enregistrer les modifications</button>
+<br>
+                <div class="col-md-10">
+                    <button class="btn btn-primary " type="submit" name="submit">Enregistrer les modifications</button>
+                </div>
+                
             </form>
             </div>
         
