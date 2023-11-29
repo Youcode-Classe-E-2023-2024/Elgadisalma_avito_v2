070be5b5ec6b329ecd22062ab6791a019b36f922
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-29 20:34:34 +0100

    Update edit.php

--- CLIENT/pages/edit.php
+++ CLIENT/pages/edit.php
@@ -18,37 +18,38 @@ $row = mysqli_fetch_assoc($result);
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
     <title>Avito</title>
 </head>
 <body>
-
-<form class="row g-3 needs-validation" action="../include/edit_traitement.php" method="post">
+    <?php
+require_once 'navbar.php';?>
+<div class="container mt-5">
+    <form class="row g-3 needs-validation" action="../include/edit_traitement.php" method="post">
     <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-    <div class="col-md-4">
-        <label for="validationCustom01" class="form-label">Numéro de téléphone</label>
+    <div class="input-group mb-3">
         <input type="text" class="form-control" id="validationCustom01" name="numero_tel" value="<?php echo $row['numero_tel'] ?>" required>
     </div>
 
-    <div class="col-md-4">
-        <label for="validationCustom02" class="form-label">Titre de l'annonce</label>
+    <div class="input-group mb-3">
         <input type="text" class="form-control" id="validationCustom02" name="titre_annonce" value="<?php echo $row['titre_annonce'] ?>" required>
     </div>
 
-    <div class="col-md-4">
-        <label for="validationCustom02" class="form-label">Description de l'annonce</label>
+    <div class="input-group mb-3">
         <input type="text" class="form-control" id="validationCustom03" name="description_annonce" value="<?php echo $row['description_annonce'] ?>" required>
     </div>
 
-    <div class="col-md-3">
-        <label for="validationCustom04" class="form-label">État de l'annonce</label>
+    <div class="input-group mb-3">
         <select class="form-select" id="validationCustom04" name="etat_annonce" required>
             <option <?php echo ($row['etat_annonce'] == 'à vendre') ? 'selected' : ''; ?>>à vendre</option>
             <option <?php echo ($row['etat_annonce'] == 'vendu') ? 'selected' : ''; ?>>vendu</option>
         </select>
     </div>
 
-    <button class="btn btn-primary" type="submit" name="submit">Enregistrer les modifications</button>
+    <button class="btn btn-outline-secondary" type="submit" name="submit">Enregistrer les modifications</button>
 </form>
+
+</div>
 
 </body>
 </html>
